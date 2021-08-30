160c99047b25d11fcd8688c0d75279102c9062e8
shahruk25 <shahruk25@gmail.com>
2021-08-30 18:30:59 +0530

    Match Anything with Wildcard Period

---
+++ tut3.js
@@ -0,0 +1,8 @@
+//Match Anything with Wildcard Period
+let humStr1 = "I'll hum a song";
+let hugStr2 = "Bear hug"
+let huRegex = /hu./;
+
+let runStr = "Run out of time."
+let runRegx=/.un/
+console.log("runRegx",runStr.match(runRegx));
