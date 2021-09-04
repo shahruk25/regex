42205097bacb532e4d349e572d1598c0da84555e
shahruk25 <shahruk25@gmail.com>
2021-09-04 21:26:34 +0530

    Match Beginning String Patterns

---
+++ tut11.js
@@ -0,0 +1,11 @@
+let firstString = "Ricky is first and can be found.";
+let firstRegex = /^Ricky/;
+firstRegex.test(firstString);
+let notFirst = "You can't find Ricky now.";
+firstRegex.test(notFirst);
+
+let rickyAndCal = "Cal and Ricky both like racing.";
+let calRegex = /^Cal/; // Change this line
+let result = calRegex.test(rickyAndCal);
+console.log(calRegex.test(rickyAndCal));
+console.log(rickyAndCal.match(calRegex));
