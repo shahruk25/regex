3eceff249a1fa062ddd3f06c0a8f923e191505ce
shahruk25 <shahruk25@gmail.com>
2021-09-04 18:43:20 +0530

    updated comment

--- tut2.js
+++ tut2.js
@@ -1,5 +1,5 @@
 // regex get repeated word;
 let testString = "Repeat, Repeat, repeat";
-let ourRegx = /Repeat/ig; // "g" =  or and i = ignore case  
+let ourRegx = /Repeat/ig; // "g" = repeatation  or and i = ignore case  
 let result = testString.match(ourRegx); // here match will retrn matched in the String literal
 console.log("ourRegx", result.length);
